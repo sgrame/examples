3315e8b6741566a56358da3af6ec3e2f86159cde
Peter Decuyper <peter@serial-graphics.be>
2012-03-06 22:54:29 +0100

    Restructured folders. Added missing unit tests

---
+++ tests/examples/controllers/FormControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class Examples_FormControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+
+    public function setUp()
+    {
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        parent::setUp();
+    }
+
+    public function testIndexAction()
+    {
+        $params = array('action' => 'index', 'controller' => 'Form', 'module' => 'examples');
+        $urlParams = $this->urlizeOptions($params);
+        $url = $this->url($urlParams);
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($urlParams['module']);
+        $this->assertController($urlParams['controller']);
+        $this->assertAction($urlParams['action']);
+        $this->assertQueryContentContains(
+            'div.page-header h1',
+            'Test form'
+        );
+    }
+
+
+}
+
+
+
---
+++ tests/examples/controllers/MessengerControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class Examples_MessengerControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+
+    public function setUp()
+    {
+        $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        parent::setUp();
+    }
+
+    public function testIndexAction()
+    {
+        $params = array('action' => 'index', 'controller' => 'Messenger', 'module' => 'examples');
+        $urlParams = $this->urlizeOptions($params);
+        $url = $this->url($urlParams);
+        $this->dispatch($url);
+        
+        // assertions
+        $this->assertModule($urlParams['module']);
+        $this->assertController($urlParams['controller']);
+        $this->assertAction($urlParams['action']);
+        $this->assertQueryContentContains(
+            'div.page-header h1',
+            'Test messenger'
+        );
+    }
+
+
+}
+
+
+
